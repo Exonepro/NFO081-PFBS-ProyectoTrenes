4cc4ae818fcbc6e5252e2dd8fea678cceb8618fd
cheric89 <cheericmc1112@gmail.com>
2025-11-17 15:27:08 -0300

    bentana cargar :V

--- ui/tempCodeRunnerFile.py
+++ ui/tempCodeRunnerFile.py
@@ -1,14 +1,1 @@
-import tkinter as tk
-
-def abrir_simulacion(root):
-    # Cerrar ventana menu primero
-    root.destroy()
-
-    sim = tk.Tk()
-    sim.title("Simulación de Trenes")
-    sim.geometry("600x400")
-
-    label = tk.Label(sim, text="Aquí irá la simulación", font=("Arial", 16))
-    label.pack(pady=20)
-
-    sim.mainloop()
+1e1e2e
--- ui/ventana_simulacion.py
+++ ui/ventana_simulacion.py
@@ -1,9 +1,30 @@
 import tkinter as tk
 from tkinter import messagebox as msgbox
+from tkinter import filedialog
 
 def ventana_simu():
+    def handler_cargar():
+        ruta = filedialog.askopenfilename(
+            title="Seleccione un archivo",
+            filetypes=[("Archivos", "*.txt"),("Todos los archivos", "*.*")]
+        )
+
+        if ruta:
+            estadocarg.config(text=f"Archivo cargado:\n{ruta}")
+        else:
+            estadocarg.config(text="No se seleccionó archivo")
+
     
     ventana = tk.Toplevel()
-    ventana.title("aña")
-    tk.Label (ventana, text="Hola ",       
-    padx=10, pady=60).pack()
+    ventana.title("Cargar simulación")
+    ventana.geometry("600x400")
+    ventana.configure(bg="#0F2661")
+    estadocarg=tk.Label (ventana, text="Cargar estado", font=("Arial", 35),
+                          bg="#0F2661",fg="black",
+                          width=50,
+                         height=3).pack()
+    
+    cargar=tk.Button(ventana, text="Seleccione el archivo a cargar.",
+                      font=("Arial",15),
+                      bg="white", command=handler_cargar).pack()
+    cargar.pack(pady=20)
